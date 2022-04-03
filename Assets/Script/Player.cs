fc5e48a86e1ad6988fdb03ea61f0c1882ad669ef
anmsg98 <wjstjd1004@naver.com>
2022-04-03 16:37:05 +0900

    [코드추가] 공격 애니메이션(LCTRL or 좌클릭) 추가 및 공격 애니메이션에 따른 카메라 셰이킹 일부 구현

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,21 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    private float hAxis;
-    private float vAxis;
+    // 카메라 셰이킹 이펙트
+    public CameraShake CameraShake;
+    public float duration;
+    public float magnitude= 0.01f;
     
-    private Vector3 moveVec;
+    // 캐릭터 움직임 및 애니메이션
+    public float speed;
     private Animator _anim;
     private CharacterController _controller;
     private Camera cam;
-
+    
+    // 캐릭터 공격
+    private float elapsed;
+    public float shakingDelay;
+    private bool attackReady;
+    
+    // 카메라 회전
     public bool toggleCameraRotation;
     public float smoothness = 10f;
+    
+    // 키 입력
+    bool fDown;
+    void GetInput()
+    {
+        fDown = Input.GetButton("Fire1");
+    }
     private void Awake()
     {
         _anim = this.GetComponent<Animator>();
@@ -26,6 +43,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Attack();
+        Move();
+        
         if (Input.GetKey(KeyCode.LeftAlt))
         {
             toggleCameraRotation = true;
@@ -34,16 +55,6 @@ public class Player : MonoBehaviour
         {
             toggleCameraRotation = false;
         }
-
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-
-        Vector3 moveDirection = forward * Input.GetAxisRaw("Vertical") + right * Input.GetAxisRaw("Horizontal");
-
-        _controller.Move(moveDirection.normalized * speed * Time.deltaTime);
-        
-        _anim.SetBool("IS_RUN", moveDirection != Vector3.zero);
-
     }
 
     private void LateUpdate()
@@ -53,6 +64,39 @@ public class Player : MonoBehaviour
             Vector3 playerRotate = Vector3.Scale(cam.transform.forward, new Vector3(1, 0, 1));
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerRotate),
                 Time.deltaTime * smoothness);
+        }
+    }
+
+    void Move()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        Vector3 moveDirection = forward * Input.GetAxisRaw("Vertical") + right * Input.GetAxisRaw("Horizontal");
+
+        _controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        
+        _anim.SetBool("IS_RUN", moveDirection != Vector3.zero);
+        print(moveDirection);
+    }
+    void Attack()
+    {
+        if (fDown)
+        {
+            elapsed = 0;
+            attackReady = true;
+            _anim.SetTrigger("DO_ATTACK");
+        }
+       
+        if (attackReady)
+        {
+            elapsed += Time.deltaTime;
+            if (shakingDelay < elapsed)
+            {
+                StartCoroutine(CameraShake.Shake(duration, magnitude));
+                attackReady = false;
+            }
+        
         }
     }
 }
