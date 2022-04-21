a3ffcb3ff91024f40180d6cb50a9cb23de8978a3
JeonSeongHee <wjstjd1004@naver.com>
2022-04-21 21:39:40 +0900

    [코드 추가] 스킬 오브젝트 동적으로 삭제 및 스킬에 collider를 넣어 몬스터가 충돌되게 하였음.

---
+++ Assets/Script/GameObjects/DestroyObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    // Update is called once per frame
+    IEnumerator Destroy()
+    {
+        WaitForSeconds destroyTick = new WaitForSeconds(3.0f);
+        yield return destroyTick;
+        Destroy(gameObject);
+    }
+}
